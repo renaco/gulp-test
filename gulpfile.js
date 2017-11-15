c113888bf282bbd1c8087e45dfa1d9fefcaf33b5
Renan Romero <renanromero@gmail.com>
2017-11-15 15:57:23 -0500

    wip(task) define pug and inline tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
+var styleInject = require("gulp-style-inject");
+var pug = require("gulp-pug");
+
+gulp.task('pug', function () {
+  return gulp.src('./source/views/*.pug')
+    .pipe(pug({
+      pretty: false
+    }))
+    .pipe(gulp.dest('./out'));
+});
  
 gulp.task('inline', function () {
   return gulp.src('./source/*.html')
     .pipe(inlinesource())
+    .pipe(styleInject({
+      encapsulated: true,
+      path: './source/'
+    }))
     .pipe(gulp.dest('./out'));
 });
