7a383be2ef6b31e9b4d94db478309b7c470fa31a
Renan Romero <renanromero@gmail.com>
2017-11-14 19:10:11 -0500

    wip(site) try to add inline

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
  
 gulp.task('inline', function () {
-    return gulp.src('./source/*.html')
-        .pipe(inlinesource())
-        .pipe(gulp.dest('./out'));
+  return gulp.src('./source/*.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./out'));
 });
